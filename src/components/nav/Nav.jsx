89cd5f6b9caa57a0d1a0248ddc8155d1ab52f8a1
leesuyeon <lgh0385hh@gmail.com>
2024-03-22 18:33:02 +0900

    FEAT: 프로필 드롭다운 구현

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,14 +4,16 @@ import { IoPersonCircleOutline } from "react-icons/io5";
 import { FaBell } from "react-icons/fa";
 import { useState } from 'react';
 import Alert from '../alert/Alert';
+import Profile from '../profile/Profile';
 
 
 const Nav = () => {
   const [alert, setAlert] = useState(false);
+  const [profile, setProfile] = useState(false);
 
   useEffect(() => {
     console.log('리렌더링')
-  }, [alert])
+  }, [alert, profile])
   
 
   return (
@@ -26,9 +28,13 @@ const Nav = () => {
 
 
   <ul className='flex text-lg  font-bold space-x-5 text-white justify-center items-center'>
+    {/* 알림 드롭다운 버튼 */}
     <li>
       <div className='relative flex justify-center'>
-        <button onClick={() => {setAlert(!alert)}}>
+        <button onClick={() => {setAlert(!alert)
+          if(profile == true) {
+            setProfile(false)
+          }}}>
         <FaBell className='h-[40px] w-[40px]' />
         {!alert && (<div className='absolute rounded-full w-3 h-3 bg-red-600 top-0 right-0'></div>)}
         </button>
@@ -37,12 +43,19 @@ const Nav = () => {
         )}
       </div>
     </li>
-    
+    {/* 마이페이지 버튼 */}
     <li>
-      <button
-        onClick={() => {window.location.href='/mypage';}}>
+      <div className='relative flex justify-center'>
+        <button onClick={() => {setProfile(!profile)
+          if(alert == true) {
+            setAlert(false)
+          }
+        }}>
         <IoPersonCircleOutline className='h-[50px] w-[50px]'/>
-      </button>
+        </button>
+        {profile && (<Profile />)}
+      </div>  
+     
     </li>
   </ul>
 </nav>
